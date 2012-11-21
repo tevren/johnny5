c6c4bf3079b673a3ace16e4141e505fe0658e2f9
Anurag Mohanty <anurag@columbia.edu>
2012-11-21 13:59:32 -0500

    removed _ad ad_ ad- -ad
    these are too small, they trip up valid selectors..

--- lib/johnny5/base.rb
+++ lib/johnny5/base.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 module Johnny5
 	class Base
-		LIST_OF_BAD_WORDS = %w{trend gallery bar submit ads-inarticle sponsor shopping widget tool promo shoutbox masthead foot footnote combx com- menu side comments comment bookmarks social links ads related similar footer digg totop metadata sitesub nav sidebar commenting options addcomment leaderboard offscreen job prevlink prevnext navigation reply-link hide hidden sidebox archives vcard tab hyperpuff chicklets trackable advertisement email partners crumbs header share discussion popup _ad ad_ ad- -ad extra community disqus about signup feedback byline trackback login blogroll rss}.uniq
+		LIST_OF_BAD_WORDS = %w{trend gallery bar submit ads-inarticle sponsor shopping widget tool promo shoutbox masthead foot footnote combx com- menu side comments comment bookmarks social links ads related similar footer digg totop metadata sitesub nav sidebar commenting options addcomment leaderboard offscreen job prevlink prevnext navigation reply-link hide hidden sidebox archives vcard tab hyperpuff chicklets trackable advertisement email partners crumbs header share discussion popup extra community disqus about signup feedback byline trackback login blogroll rss}.uniq
 		TAGS_TO_EXAMINE = %w{div p table tr td pre tbody section ul ol li}
 		TAGS_TO_REMOVE = %w{header footer nav script noscript form input}
 		def initialize(url)
